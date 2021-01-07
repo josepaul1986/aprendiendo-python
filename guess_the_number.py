d15d72db724d8fdf6fcb6b08230957a12c43b7b2
Paul Alvarado <josepaul_ar@outlook.com>
2021-01-06 20:19:49 -0600

    Se corrigio los ciclos de intentos y repeticion de juego.

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,38 +2,44 @@ import random
 
 random_number = random.randint(1,10)
 
-print("Welcome to \"Guess the number?\" Game!")
+print("\n\nWelcome to \"Guess the number?\" Game!")
 print("\nI'm thinking in a number between 1 and 10.")
-print("\nCould you tell me which is?")
+print("\nCould you tell me which number is?")
 
 times = 0
+repeat = "y"
 
-while True:
-    guessing = int(input("\nWhich number is?: "))
+while repeat == "y":
 
-    times += 1
+    while times < 5:
 
-    if guessing == random_number:
-        print("\nYour guess is TRUE! Congrats!")
-        break
-    else:
-        diff = guessing - random_number
-        if abs(diff) == 1:
-            print("\nFail! Hot! Try again!")
-        elif abs(diff) == 2:
-            print("\nFail! Warm! Try again!")
-        elif abs(diff) == 3:
-            print("\nFail! Warming up! Try again!")
-        else:
-            print("\nFail! You're cold! Try again!")
+        times += 1
+        
+        guessing = int(input("\nWhich number do you think it is?: "))
 
-    if times == 5:
-        repeat = input("\nDo you want to play again? [Yes/No]")
-        repeat = repeat.lower()
-        if repeat == "y" or repeat == "yes":
-            random_number = random.randint(1,10)
-            continue
-        else:
+        if guessing == random_number:
+            print("\nYour guess is TRUE! Congrats!")
             break
+        else:
+            diff = guessing - random_number
+            if abs(diff) == 1:
+                print("\nFail! Hot! Try again!")
+            elif abs(diff) == 2:
+                print("\nFail! Warm! Try again!")
+            elif abs(diff) == 3:
+                print("\nFail! Warming up! Try again!")
+            else:
+                print("\nFail! You're cold! Try again!")
+        
+        print("\nYou have {0} tries left! Keep goin'!".format(5-times))
+
+    repeat = input("\nDo you want to play again? [Yes/No]: ")
+    repeat = repeat.lower()
+
+    if repeat == "y" or repeat == "yes":
+        times = 0
+        continue
+    else:
+        break
 
 print("\nThanks for playing!\n")
