6cdb7d003c542f279537c9ea8adbc57637167fc5
Paul Alvarado <josepaul_ar@outlook.com>
2021-01-12 18:07:39 -0600

    Se hace un ejemplo de herencia basica.

--- POO/player.py
+++ POO/player.py
@@ -1,10 +1,31 @@
 class Player:
+    vocacion = "Aldeano Valiente"
+    hechizo = "Puff!"
+    velocidad_movimiento = 50
 
     def __init__(self, **kwargs):
         self.hit_points = kwargs.get("hit_points",50)
         self.mana = kwargs.get("mana",50)
-        self.vocacion = kwargs.get("vocacion","gnomo")
-        self.hechizo = kwargs.get("hechizo","Puff!")
 
     def lanzar_hechizo(self):
         return self.hechizo
+
+class Sorcerer(Player):
+    vocacion="Sorcerer"
+    hechizo = "Wing guardium leviousa!"
+    velocidad_movimiento = 20
+
+class Knight(Player):
+    vocacion="Caballero"
+    hechizo = "Expelermus!"
+    velocidad_movimiento = 60
+
+class Paladin(Player):
+    vocacion="Caballero"
+    hechizo = "Espectro Patronus!"
+    velocidad_movimiento = 80
+
+class Druida(Player):
+    vocacion="Druida"
+    hechizo = "Exori!"
+    velocidad_movimiento = 45
