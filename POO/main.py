6677aee6a2d9b79b49c1824900bf9edee2bf9393
Paul Alvarado <josepaul_ar@outlook.com>
2021-01-15 16:38:48 -0600

    Se implemento el metodo __str__ en la clase player.

--- POO/main.py
+++ POO/main.py
@@ -6,27 +6,8 @@ knight = player.Knight(hit_points=80,mana=20)
 paladin = player.Paladin(hit_points=100,mana=40)
 druid = player.Druida(hit_points=60,mana=100)
 
-print("\nEl jugador tiene vocacion {0} con {1} puntos de ataque. Puede lanzar el hechizo {2} y puede moverse a una velocidad {3}".format(gnomo.vocacion, 
-                                                                                                                                        gnomo.hit_points,
-                                                                                                                                        gnomo.lanzar_hechizo(), 
-                                                                                                                                        gnomo.velocidad_movimiento))
-
-print("\nEl jugador tiene vocacion {0} con {1} puntos de ataque. Puede lanzar el hechizo {2} y puede moverse a una velocidad {3}".format(sorcerer.vocacion, 
-                                                                                                                                        sorcerer.hit_points,
-                                                                                                                                        sorcerer.lanzar_hechizo(), 
-                                                                                                                                        sorcerer.velocidad_movimiento))
-
-print("\nEl jugador tiene vocacion {0} con {1} puntos de ataque. Puede lanzar el hechizo {2} y puede moverse a una velocidad {3}".format(knight.vocacion, 
-                                                                                                                                        knight.hit_points,
-                                                                                                                                        knight.lanzar_hechizo(), 
-                                                                                                                                        knight.velocidad_movimiento))
-
-print("\nEl jugador tiene vocacion {0} con {1} puntos de ataque. Puede lanzar el hechizo {2} y puede moverse a una velocidad {3}".format(paladin.vocacion, 
-                                                                                                                                        paladin.hit_points,
-                                                                                                                                        paladin.lanzar_hechizo(), 
-                                                                                                                                        paladin.velocidad_movimiento))
-
-print("\nEl jugador tiene vocacion {0} con {1} puntos de ataque. Puede lanzar el hechizo {2} y puede moverse a una velocidad {3}".format(druid.vocacion, 
-                                                                                                                                        druid.hit_points,
-                                                                                                                                        druid.lanzar_hechizo(), 
-                                                                                                                                        druid.velocidad_movimiento))
+print(gnomo)
+print(sorcerer)
+print(knight)
+print(paladin)
+print(druid)
--- POO/player.py
+++ POO/player.py
@@ -3,9 +3,15 @@ class Player:
     hechizo = "Puff!"
     velocidad_movimiento = 50
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs): #metodo constructor que recibe argumento multiple tipo diccionario
         self.hit_points = kwargs.get("hit_points",50)
         self.mana = kwargs.get("mana",50)
+
+    def __str__(self): # metodo str sobreescrito para
+        return "\nEl jugador tiene vocacion {0} con {1} puntos de ataque. Puede lanzar el hechizo {2} y puede moverse a una velocidad {3}".format(self.vocacion, 
+                                                                                                                                        self.hit_points,
+                                                                                                                                        self.lanzar_hechizo(), 
+                                                                                                                                        self.velocidad_movimiento)
 
     def lanzar_hechizo(self):
         return self.hechizo
